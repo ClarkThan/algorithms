49e89261094ecdc91b974422952c830bb8cab80d
Clark Than <lenplex@gmail.com>
2015-06-24 14:41:44 +0800

    add InsertSort.py

---
+++ sort/InsertSort.py
@@ -0,0 +1,23 @@
+#InsertSort
+def insertSort(seq):
+	length = len(seq)
+	for i in range(1, length):
+		key = seq[i]
+		j = i - 1
+		while j >= 0 and seq[j] > key:
+			seq[j+1] = seq[j]
+			j = j - 1
+		seq[j+1] = key
+		
+	return seq
+	
+a = []
+b = [-1]
+c = [3, 3]
+d = [11,-2,-5,21,10,3,7,33,0,13,5,-7,22,41,28,45]
+
+if __name__ == '__main__':
+	print insertSort(a)
+	print insertSort(b)
+	print insertSort(c)
+	print insertSort(d)
